5f75d472142041034563bd51bf491310184d6911
Thabiso Malematja <Thabiso.Malematja@reverside.co.za>
2022-06-07 13:13:21 +0200

    Queried data from the database in the Restaurant details and restaurant screens

--- src/screens/RestaurantDetails/Header.js
+++ src/screens/RestaurantDetails/Header.js
@@ -2,14 +2,18 @@ import { Text, Image, View} from 'react-native';
 import restaurants from '../../data/restaurants.json';
 import styles from './styles'; 
 
+const DEFAULT_PICTURE = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/uber-eats/restaurant1.jpeg'; 
+
+
 const RestaurantHeader = ({restaurant}) =>{
     return (
         <View style={styles.page}>
-            <Image source={{uri: restaurant.image}} style={styles.image} />
+            <Image source={{uri: restaurant.image.startsWith('http') ? restaurant.image : DEFAULT_PICTURE}} 
+            style={styles.image} />
 
             <View style={styles.container}>
                 <Text style={styles.title}>{restaurant.name}</Text>
-                <Text style={styles.subtitle}>R{restaurant.deliveryFee} &#8226; 
+                <Text style={styles.subtitle}>R{restaurant.deliveryFee.toFixed(2)} &#8226; 
                     {restaurant.minDeliveryTime}-{restaurant.maxDeliveryTime} minutes</Text>
                 <Text style={styles.menuTitle}>Menu</Text>
             </View> 
--- src/screens/RestaurantDetails/Index.js
+++ src/screens/RestaurantDetails/Index.js
@@ -1,27 +1,39 @@
-import { View, FlatList} from 'react-native';
-import restaurants from '../../data/restaurants.json';
+import { View, FlatList, ActivityIndicator} from 'react-native';
 import {Ionicons} from '@expo/vector-icons';
 import DishListItem from '../../components/DishListItem/Index';
 import Header from './Header';
 import styles from './styles';
 import { useRoute, useNavigation } from '@react-navigation/native';
-
-const restaurant = restaurants[0];
+import {useState, useEffect} from 'react';
+import { DataStore } from 'aws-amplify';
+import {Restaurant, Dish} from '../../models/index';
 
 const RestaurantDetailsPage = () =>{
+
+    const [restaurant, setRestaurant] = useState(null);
+    const [dishes, setDish] = useState([]);
 
     const route = useRoute();
     const navigation = useNavigation();
 
     const id = route.params?.id; 
-    console.warn(id);
+
+    useEffect(() =>{
+        DataStore.query(Restaurant, id).then(setRestaurant);
+
+        DataStore.query(Dish, (dish) => dish.restaurantID('eq', id)).then(setDish);
+    },[]);
+
+    if(!restaurant){
+        return <ActivityIndicator size={'large'} style={{paddingVertical: 350}}/>
+    }
 
     return (
         <View style={styles.page}>
 
             <FlatList 
                 ListHeaderComponent={() => <Header restaurant={restaurant} />}
-                data={restaurant.dishes}
+                data={dishes}
                 renderItem = {({item}) => <DishListItem dish={item} 
                 keyExtractor={(item) => item.name} />}
             />
